5904d84726be08159635b42068ac19abebf03d77
theNoobCoder123 <patelhrishikesh2000@gmail.com>
2022-01-21 01:27:53 +0530

    Created directive-pipes component. Generated component directive-pipes and and a custon directive firstUpper

---
+++ src/app/directive-pipes/directive-pipes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directive-pipes',
+  templateUrl: './directive-pipes.component.html',
+  styleUrls: ['./directive-pipes.component.css']
+})
+export class DirectivePipesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/directive-pipes/directive-pipes.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DirectivePipesComponent } from './directive-pipes.component';
+
+
+
+@NgModule({
+  declarations: [
+    DirectivePipesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DirectivePipesModule { }
