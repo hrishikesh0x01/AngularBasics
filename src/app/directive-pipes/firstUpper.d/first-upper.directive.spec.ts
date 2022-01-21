f14a03c41f1de35e47dc640b778ddb13a66c8812
Hrishikesh Patel <49692779+theNoobCoder123@users.noreply.github.com>
2022-01-21 16:18:05 +0530

    Delete src/app/directive-pipes/firstUpper.d directory

--- src/app/directive-pipes/firstUpper.d/first-upper.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FirstUpperDirective } from './first-upper.directive';
-
-describe('FirstUpperDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FirstUpperDirective();
-    expect(directive).toBeTruthy();
-  });
-});
